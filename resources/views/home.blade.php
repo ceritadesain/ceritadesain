5ce993a8edaf348bac0f0860a94bcd9317f9d769
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-06-17 00:24:57 +0700

    fix UI homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,14 +56,14 @@
 
     {{-- HERO --}}
     <section class="container-fluid hero ">
-        <div class="row align-items-center h-100 text-center  ">
-            <div class="col-12 col-lg-12  ">
+        <div class="row align-items-center h-100 text-center px-lg-5 ">
+            <div class="col-12">
                 <div class=" hero-text">
                     <h1 class="text-white">Selamat Datang di DesignSpeak <br>
                         Komunitas untuk Para Penggemar UI/UX</h1>
                 </div>
-                <div class=" hero-text pt-2">
-                    <p class="mb-4 text-white  ">Selamat Datang di CeritaDesain, tempat kreativitas bertemu dengan
+                <div class="hero-text pt-2 ">
+                    <p class="mb-4 text-white">Selamat Datang di CeritaDesain, tempat kreativitas bertemu dengan
                         kolaborasi
                         dalam
                         dunia desain UI/UX yang dinamis. Terjunlah ke dalam komunitas yang bersemangat, di mana desainer
@@ -77,12 +77,13 @@
     </section>
 
     {{-- PROMOTION SECTION --}}
-    <section class="container height-promotion text-white d-flex align-items-center justify-content-center">
+    <section
+        class="container py-80px text-white custom-padding d-flex align-items-center justify-content-center text-center text-lg-start">
         <div class="row w-100 align-items-center">
             <div class="col-12 col-lg-6 ">
                 <h2>Gabung <br> Komunitas Kami</h2>
             </div>
-            <div class="col-12 col-lg-6 d-flex flex-column align-items-center">
+            <div class="col-12 col-lg-6 ">
                 <div class="col-12 col-lg-4 ">
                     <h2 class="font-extra-extra-large">51,875</h2>
                     <p class="font-small">Diskusi</p>
@@ -100,42 +101,41 @@
     </section>
 
     {{-- INFOGRAPHIC SECTION --}}
-    <section class="container height-promotion text-white d-flex align-items-center justify-content-center">
-        <div class="container text-center">
-            <div class="row p-2 p-lg-5">
-                <div class="col-lg-6 col-sm-12   d-flex align-items-center justify-content-center ">
-                    <div>
-                        <h2>Kolaborasi Komunitas</h2>
-                        <p>Dengan menggunakan platform CeritaDesain, Anda dapat terhubung dengan komunitas luas dari
-                            profesional dan penggemar UI/UX. Mulai dari desainer senior hingga pemula, platform ini
-                            menyediakan ruang untuk kolaborasi, pertukaran ide, dan umpan balik yang berharga.</p>
-                    </div>
+    <section class="container py-80px text-white custom-padding ">
+        <div class="row p-2 p-lg-5">
+            <div class="col-lg-6 col-sm-12 d-flex align-items-center justify-content-center   ">
+                <div class="text-center">
+                    <h2>Kolaborasi Komunitas</h2>
+                    <p>Dengan menggunakan platform CeritaDesain, Anda dapat terhubung dengan komunitas luas dari
+                        profesional dan penggemar UI/UX. Mulai dari desainer senior hingga pemula, platform ini
+                        menyediakan ruang untuk kolaborasi, pertukaran ide, dan umpan balik yang berharga.</p>
                 </div>
-                <div class="col-lg-6 col-sm-12   order-first order-lg-last">
-                    <img class="info-image pb-2" src="{{ url('assets/images/image-1.png') }}" alt="image-1">
-                </div>
-
             </div>
-            <div class="row p-lg-5">
-                <div class="col-lg-6 col-sm-12 ">
-                    <img class="info-image pb-2" src="{{ url('assets/images/image-2.png') }}" alt="image-2">
-                </div>
-                <div class="col-lg-6 col-sm-12 d-flex align-items-center justify-content-center ">
-                    <div>
-                        <h2>Pusat Sumber Informasi</h2>
-                        <p>CeritaDesain juga berfungsi sebagai pusat sumber daya lengkap untuk segala hal yang berkaitan
-                            dengan UI/UX. Anda akan menemukan artikel, tutorial, dan sumber daya lain yang berguna yang
-                            dibagikan oleh anggota komunitas. Mulai dari tren terbaru dalam desain antarmuka hingga
-                            teknik pengujian pengguna, platform ini memberikan akses ke informasi terkini dan dapat
-                            diandalkan.</p>
-                    </div>
+            <div class="col-lg-6 col-sm-12 order-first order-lg-last d-flex align-items-center justify-content-center">
+                <img class="info-image pb-2" src="{{ url('assets/images/image-1.png') }}" alt="image-1">
+            </div>
+
+        </div>
+        <div class="row p-lg-5 pt-80px">
+            <div class="col-lg-6 col-sm-12  d-flex align-items-center justify-content-center">
+                <img class="info-image pb-2" src="{{ url('assets/images/image-2.png') }}" alt="image-2">
+            </div>
+            <div class="col-lg-6 col-sm-12 d-flex align-items-center justify-content-center ">
+                <div class="text-center">
+                    <h2>Pusat Sumber Informasi</h2>
+                    <p>CeritaDesain juga berfungsi sebagai pusat sumber daya lengkap untuk segala hal yang berkaitan
+                        dengan UI/UX. Anda akan menemukan artikel, tutorial, dan sumber daya lain yang berguna yang
+                        dibagikan oleh anggota komunitas. Mulai dari tren terbaru dalam desain antarmuka hingga
+                        teknik pengujian pengguna, platform ini memberikan akses ke informasi terkini dan dapat
+                        diandalkan.</p>
                 </div>
             </div>
         </div>
+
     </section>
 
     {{-- Help Others SECTION --}}
-    <section class=" container height-promotion text-white d-flex align-items-center justify-content-center">
+    <section class=" container py-80px text-white">
         <div class="container py-80px">
             <h2 class="text-center mb-5">Bantu Sesama</h2>
             <div class="row">
@@ -233,7 +233,8 @@
         </div>
     </section>
     {{-- CTA --}}
-    <section class="container min-h-372px text-white d-flex flex-column align-items-center justify-content-center">
+
+    <section class="container py-80px d-flex flex-column align-items-center justify-content-center text-white">
         <h2>Siap untuk berkontribusi?</h2>
         <p class="mb-4">Ingin membuat dampak besar?</p>
         <div class="text-center">
@@ -241,6 +242,8 @@
             <a href="#" class="btn btn-secondary mb-2 mb-lg-0">Gabung Diskusi Sekarang!</a>
         </div>
     </section>
+
+
     {{-- footer --}}
     <footer class="py-80px">
         <div class="container">
