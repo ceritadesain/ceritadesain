0d7b7ce685bdf72dff257802bfe7d7a684e1173c
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-06-16 17:41:25 +0700

    Create UI for homepage cta and footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -232,8 +232,45 @@
             </div>
         </div>
     </section>
+    {{-- CTA --}}
+    <section class="container min-h-372px text-white d-flex flex-column align-items-center justify-content-center">
+        <h2>Siap untuk berkontribusi?</h2>
+        <p class="mb-4">Ingin membuat dampak besar?</p>
+        <div class="text-center">
 
-
+            <a href="#" class="btn btn-secondary mb-2 mb-lg-0">Gabung Diskusi Sekarang!</a>
+        </div>
+    </section>
+    {{-- footer --}}
+    <footer class="py-80px">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-6 mb-5 mb-lg-0">
+                    <img src="{{ url('assets/images/ceritadesain-logo.png') }}" alt="CeritaDesain Logo"
+                        class="h-32px mb-4">
+                    <p class="text-white">Menguatkan Ide, Membangkitkan Percakapan UI/UX.</p>
+                </div>
+                <div class="col-12 col-lg-6 me-auto">
+                    <div class="d-flex flex-column flex-lg-row justify-content-end ">
+                        <div class="d-flex flex-column me-140px mb-3 mb-lg-0">
+                            <p class="fw-bold fs-5 text-white text-nowrap">Kontak kami</p>
+                            <ul class="list-unstyled">
+                                <li class="text-white"><a href="mailto:hi@laracuss.com">hi@ceritadesain.com</a></li>
+                            </ul>
+                        </div>
+                        <div class="d-flex flex-column">
+                            <p class="fw-bold fs-5 text-white">Links</p>
+                            <ul class="list-unstyled">
+                                <li class="text-white mb-2"><a href="#">Beranda</a></li>
+                                <li class="text-white mb-2"><a href="#">Diskusi</a></li>
+                                <li class="text-white mb-2"><a href="#">Tentang kami</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 </body>
