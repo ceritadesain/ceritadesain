c644c11d8a8add11e29856af29850808fa7e01d6
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-07-01 23:38:00 +0700

    fix: url favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="icon" href="assets/images/favicon.ico" type="image/x-icon">
+<link rel="icon" href="{{ url('assets/images/favicon.ico') }}" type="image/x-icon">
 <title>CeritaDesain</title>
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
 @vite(['resources/scss/app.scss', 'resources/js/app.js'])
